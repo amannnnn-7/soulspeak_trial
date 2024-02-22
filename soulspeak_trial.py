19224a548bf9c0e03c3d7c2a90805618d14281f6
Aman <79796357+amannnnn-7@users.noreply.github.com>
2024-02-22 19:03:09 +0530

    Update soulspeak_trial.py

--- soulspeak_trial.py
+++ soulspeak_trial.py
@@ -1,8 +1,27 @@
 import streamlit as st
-from transformers import pipeline
+from transformers import RobertaTokenizerFast, TFRobertaForSequenceClassification, pipeline
 import whisper
 
 st.title('Emotion Detection from Audio')
+
+model = whisper.load_model('base')
+result = model.transcribe('song1.wav', fp16 = False)
+txt = result['text']
+
+tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
+model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
+
+emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+
+emotion_labels = emotion(txt)
+
+#print(emotion_labels)
+
+classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+
+model_outputs = classifier(txt)
+
+#print(model_outputs[0])
 
 audio_file = st.file_uploader("Upload an audio file", type=['wav', 'mp3', 'ogg'])
 if audio_file is not None:
