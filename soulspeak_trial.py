1759268e21ee9fe04adef518f06dd06f5173f669
Aman <79796357+amannnnn-7@users.noreply.github.com>
2024-02-22 19:53:28 +0530

    Update soulspeak_trial.py

--- soulspeak_trial.py
+++ soulspeak_trial.py
@@ -11,15 +11,15 @@ model = whisper.load_model('base')
 tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa")
 model = TFRobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa")
 
-emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
+#emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa')
 
-emotion_labels = emotion(txt)
+#emotion_labels = emotion(txt)
 
 #print(emotion_labels)
 
-classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+#classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
 
-model_outputs = classifier(txt)
+#model_outputs = classifier(txt)
 
 #print(model_outputs[0])
 
